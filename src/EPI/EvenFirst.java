05b2ddbff437e7aa5c47abf0fe0d12ab84647a6f
Bharat Verma <bharat.nitrr10@gmail.com>
2019-07-10 23:50:59 -0500

    Algorithm practice

---
+++ src/EPI/EvenFirst.java
@@ -0,0 +1,29 @@
+package EPI;
+
+public class EvenFirst {
+
+    public static void evenFirst(int[] inputArray){
+
+        int start=0, end=inputArray.length-1;
+        while(start<end){
+            if(inputArray[start]%2 != 0){
+                int temp = inputArray[start];
+                inputArray[start] = inputArray[end];
+                inputArray[end] = temp;
+                end--;
+            }
+            else {
+                start++;
+            }
+        }
+        for (int i=0; i<inputArray.length;i++){
+            System.out.println(inputArray[i]);
+        }
+    }
+
+    public static void main(String args[]){
+
+        int[] inputArray = {1,2,3,4,5,6,7,8,9};
+        EvenFirst.evenFirst(inputArray);
+    }
+}
