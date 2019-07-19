a69d919e3157e01675fb35a27be8287df3de4724
Bharat Verma <bharat.nitrr10@gmail.com>
2019-07-19 00:22:36 -0500

    Algorithm practice

---
+++ Leetcode/Leetcode_235.java
@@ -0,0 +1,23 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_235 {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return root;
+    }
+}
+
+public class Leetcode_235 {
+    public static void main(String args[]){
+        //Solution_235.lowestCommonAncestor();
+    }
+}
